f1d086d7ddcf8f7f58b867bad753be30c6ad3f91
Cristina Gaspar Samaniego <DAW1-A@PC28>
2024-03-01 17:46:19 +0100

    Commit Prac3

---
+++ Prac3_GITHub/src/ProyectoGITHub/GITHub.java
@@ -0,0 +1,18 @@
+package ProyectoGITHub;
+
+public class GITHub {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es una modificación del main");
+		modificacion2();
+		modificacion3(3);
+	}
+	
+	public static void modificacion2() {
+		System.out.println("Esto es la modificación número 2");
+	}
+	
+	public static void modificacion3(int num) {
+		System.out.println("Esta es la modificación número "+num);
+	}
+}
